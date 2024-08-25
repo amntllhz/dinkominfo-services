c5a5cf1290d375390a52d29223431bb83456e748
drchmsyh <dirchamsyah@gmail.com>
2024-08-25 16:27:39 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontController::class, 'index'])
     ->name('front.index');
+// tes;
 
 Route::get('/services', [FrontController::class, 'services'])
     ->name('front.services');
