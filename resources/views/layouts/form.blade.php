50d127d6875a83fae14c9bc772cb86d775f8cc47
drchmsyh <dirchamsyah@gmail.com>
2024-08-26 15:41:00 +0700

    add instansi fix

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('style.css') }}">
     <link href="https://api.fontshare.com/v2/css?f[]=clash-display@200,300,400,500,600,700,1&display=swap" rel="stylesheet">
     <script src="{{ asset('scripts.js') }}"></script>
